a37110971369eff007521675988b5c03ebc31092
Tiago C. dos Santos <caceraghi@gmail.com>
2024-09-20 10:26:49 -0300

    versao final

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-//Seleciona os elementos do HTML
-const buscaEntrada = document.getElementById('busca');
-const btnBusca = document.getElementById('btnBusca');
+// Selecionando os elementos HTML
+const searchInput = document.getElementById('search');
+const searchBtn = document.getElementById('searchBtn');
 const pokemonInfo = document.getElementById('pokemon-info');
 
-// Busca o Pokemon pela API - PokeAPI
-async function buscaPokemon(pokemon) {
-    try {
-        //Busca os dados da API pelo nome ou número
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemin.toLowerCAse()}');
-
-        if (response.ok) {
-            const data = await response.json();
-            displayPokemon(data);
-        }else {
-            pokemonInfo.innerHTML = '<p>Pokemon não Encontrado. Tente Novamente!<p>';
-        }
-    } catch (error){
-        pokemonInfo.innerHTML = '<p>Ocorreu um erro ao buscar o Pakemon. Verifique sua conexão.<p>';
+// Função para buscar o Pokémon pela PokéAPI
+async function fetchPokemon(pokemon) {
+  try {
+    // Buscando os dados da API pelo nome ou número
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    
+    if (response.ok) {
+      const data = await response.json();
+      
+      // Exibindo as informações do Pokémon
+      displayPokemon(data);
+    } else {
+      pokemonInfo.innerHTML = `<p>Pokémon não encontrado. Tente novamente!</p>`;
     }
+  } catch (error) {
+    pokemonInfo.innerHTML = `<p>Ocorreu um erro ao buscar o Pokémon. Verifique sua conexão.</p>`;
+  }
 }
 
-//Função que vai exibir as informações do Pokemon
+// Função para exibir as informações do Pokémon
 function displayPokemon(data) {
-    pokemonInfo.innerHTM = 'pokemonInfo.innerHTML = <img src="${data.sprites.front_default}" alt="${data.name}"><h2>${data.name} (#${data.id})</h2><p>Altura: ${(data.height / 10).toFixed(1)}m</p><p>Peso: ${(data.weight / 10).toFixed(1)}kg</p><p>Tipo: ${data.types.map(type => type.type.name).join(', ')}</p>';
+  pokemonInfo.innerHTML = `
+    <img src="${data.sprites.front_default}" alt="${data.name}">
+    <h2>${data.name} (#${data.id})</h2>
+    <p>Altura: ${(data.height / 10).toFixed(1)}m</p>
+    <p>Peso: ${(data.weight / 10).toFixed(1)}kg</p>
+    <p>Tipo: ${data.types.map(type => type.type.name).join(', ')}</p>
+  `;
 }
 
 // Adicionando evento de clique no botão de busca
-btnBusca.addEventListener('click', () => {
-    const pokemon = buscaEntrada.value.trim();
-    
-    if (pokemon) {
-        buscaPokemon(pokemon);
-    } else {
-        pokemonInfo.innerHTML = `<p>Por favor, insira um nome ou número de Pokémon.</p>`;
-    }
-  });
+searchBtn.addEventListener('click', () => {
+  const pokemon = searchInput.value.trim();
   
+  if (pokemon) {
+    fetchPokemon(pokemon);
+  } else {
+    pokemonInfo.innerHTML = `<p>Por favor, insira um nome ou número de Pokémon.</p>`;
+  }
+});
